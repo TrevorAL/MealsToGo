b0997dee9c63ba68e194197961c06d7e53e37046
TrevorAL <tlachman4@gmail.com>
2022-01-19 14:33:33 -0500

    set up foundation of restaurant popup detail

--- src/infastructure/navigation/restaurants.navigator.js
+++ src/infastructure/navigation/restaurants.navigator.js
@@ -1,17 +1,31 @@
 import React from "react";
 
-import { createStackNavigator } from "@react-navigation/stack";
+import { 
+  createStackNavigator,
+  TransitionPresets,
+} from "@react-navigation/stack";
+
+import { RestaurantDetailScreen } from "../../features/restaurants/screens/restaurant-detail.screen";
 import { RestaurantsScreen } from "../../features/restaurants/screens/restaurants.screen";
 
 const RestaurantStack = createStackNavigator();
 
 export const RestaurantsNavigator = () => {
   return (
-    <RestaurantStack.Navigator headerMode="none">
-      <RestaurantStack.Screen 
-        name="Restaurant"
+    <RestaurantStack.Navigator
+      headerMode="none"
+      screenOptions={{
+        ...TransitionPresets.ModalPresentationIOS,
+      }}
+    >
+      <RestaurantStack.Screen
+        name="Restaurants"
         component={RestaurantsScreen}
       />
+      <RestaurantStack.Screen
+        name="RestaurantDetail"
+        component={RestaurantDetailScreen}
+      />
     </RestaurantStack.Navigator>
-  )
-}
+  );
+};
