ed7165be15c44c15fea2ded17511dfb004536848
TrevorAL <tlachman4@gmail.com>
2022-01-18 19:38:16 -0500

    Theming of the style components

--- App.js
+++ App.js
@@ -1,13 +1,17 @@
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import React from "react";
+import { ThemeProvider } from "styled-components/native";
 
 import { RestaurantsScreen } from "./src/features/restaurants/screens/restaurants.screen";
+import { theme } from "./src/infastructure/theme";
 
 export default function App() {
   return (
     <>
+    <ThemeProvider theme={theme}>
       <RestaurantsScreen />
-      <ExpoStatusBar style="auto" />
+    </ThemeProvider>
+    <ExpoStatusBar style="auto" />
     </>
   );
 }
